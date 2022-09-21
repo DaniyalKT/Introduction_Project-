cb06dc471d0b160905a9f39e88ecfe0390851c3f
دانیال کفائی نژاد طهرانی <65863212+DaniyalKT@users.noreply.github.com>
2022-09-21 15:35:09 +0430

    static portfolio and added template

---
+++ Introduction_Project/Models/Projects.cs
@@ -0,0 +1,18 @@
+﻿namespace Introduction_Project.Models
+{
+    public class Projects
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Images { get; set; }
+
+        public Projects(long id, string name, string description, string images)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Images = images;
+        }
+    }
+}
---
+++ Introduction_Project/ViewComponent/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Introduction_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction_Project.ViewComponents
+{
+    public class ProjectsViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Projects>
+            {
+                new Projects(1, "سایت فروشگاهی پوتین", "این وب سایت جهت ارائه ناب ترین کفش ها، کتونی ها . پوتین های زمستانی و تابستانی خدمت شما عزیزان ارائه شده است","portfolio-2.jpg"),
+                new Projects(2, "سایت عکاسی کنان", "این وب سایت فروشگاهی جهت ارائه بهترین و با کیفیت ترین عکس های طبیعت و مراسمات شما عزیزان ارائه شده است","portfolio-7.jpg"),
+                new Projects(3, "سایت فروشگاهی ساعت های هوشمند", "برای خرید ساعت های هوشمند با بهترین قیمت و بهترین کیفیت با برند های معروف و همچنین گارانتی بلند مدت برای شما عزیزان ارائه شده است","portfolio-9.jpg"),
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
